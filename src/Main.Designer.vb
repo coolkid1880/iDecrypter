65e47644bbfb5b659e86c3d280e54bf45064a98f
Fallensn0w <thimcederlund@hotmail.com>
2010-08-24 19:27:42 +0200

    first commit

---
+++ src/Main.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.buttonSelectOutput = New System.Windows.Forms.Button
+        Me.buttonSelectInput = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.buttonDecrypt = New System.Windows.Forms.Button
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.dlgInput = New System.Windows.Forms.OpenFileDialog
+        Me.dlgOutput = New System.Windows.Forms.SaveFileDialog
+        Me.tmrChecker = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.buttonSelectOutput)
+        Me.GroupBox1.Controls.Add(Me.buttonSelectInput)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 70)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 77)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'buttonSelectOutput
+        '
+        Me.buttonSelectOutput.Location = New System.Drawing.Point(230, 45)
+        Me.buttonSelectOutput.Name = "buttonSelectOutput"
+        Me.buttonSelectOutput.Size = New System.Drawing.Size(28, 23)
+        Me.buttonSelectOutput.TabIndex = 5
+        Me.buttonSelectOutput.Text = "..."
+        Me.buttonSelectOutput.UseVisualStyleBackColor = True
+        '
+        'buttonSelectInput
+        '
+        Me.buttonSelectInput.Location = New System.Drawing.Point(230, 18)
+        Me.buttonSelectInput.Name = "buttonSelectInput"
+        Me.buttonSelectInput.Size = New System.Drawing.Size(28, 23)
+        Me.buttonSelectInput.TabIndex = 4
+        Me.buttonSelectInput.Text = "..."
+        Me.buttonSelectInput.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(79, 45)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(145, 21)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(79, 18)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(145, 21)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(6, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Output File:"
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(6, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input File:"
+        '
+        'buttonDecrypt
+        '
+        Me.buttonDecrypt.Enabled = False
+        Me.buttonDecrypt.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonDecrypt.Location = New System.Drawing.Point(12, 153)
+        Me.buttonDecrypt.Name = "buttonDecrypt"
+        Me.buttonDecrypt.Size = New System.Drawing.Size(264, 42)
+        Me.buttonDecrypt.TabIndex = 1
+        Me.buttonDecrypt.Text = "Decrypt"
+        Me.buttonDecrypt.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.TextBox3)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 4)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(264, 65)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(252, 23)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Enter the key for the encrypted disk image."
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(9, 39)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(249, 21)
+        Me.TextBox3.TabIndex = 0
+        '
+        'dlgInput
+        '
+        Me.dlgInput.Filter = "Encrypted Apple Disk Image|*.dmg"
+        '
+        'dlgOutput
+        '
+        Me.dlgOutput.FileName = "decrypted.dmg"
+        Me.dlgOutput.Filter = "Decrypted Apple Disk Image|*.dmg"
+        '
+        'tmrChecker
+        '
+        Me.tmrChecker.Enabled = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(288, 203)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.buttonDecrypt)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "iDecrypter -- By: Fallensn0w"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents buttonSelectOutput As System.Windows.Forms.Button
+    Friend WithEvents buttonSelectInput As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents buttonDecrypt As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents dlgInput As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents dlgOutput As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents tmrChecker As System.Windows.Forms.Timer
+
+End Class
---
+++ src/Main.vb
@@ -0,0 +1,80 @@
+﻿
+Public Class Main
+
+    Dim InputSelected As Boolean = False, _
+           OutputSelected As Boolean = False
+
+    ' A subroutine that will execute a .EXE and wait til its done.
+    Sub DoCMD(ByVal file As String, ByVal arg As String)
+        Dim procNlite As New Process
+        winstyle = 1
+        procNlite.StartInfo.FileName = file
+        procNlite.StartInfo.Arguments = " " & arg
+        procNlite.StartInfo.WindowStyle = winstyle
+        Application.DoEvents()
+        procNlite.Start()
+        Do Until procNlite.HasExited
+            Application.DoEvents()
+            For i = 0 To 5000000
+                Application.DoEvents()
+            Next
+        Loop
+        procNlite.WaitForExit()
+    End Sub
+
+    Private Sub buttonDecrypt_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonDecrypt.Click
+
+        tmrChecker.Enabled = False
+        buttonDecrypt.Enabled = False
+        buttonDecrypt.Text = "Decrypting..."
+
+        DoCMD("vfdecrypt.exe", _
+                                                    " -i " & Chr(34) & TextBox1.Text & Chr(34) & _
+                                                    " -k " & TextBox3.Text & " " & _
+                                                    " -o " & Chr(34) & TextBox2.Text & Chr(34))
+
+        buttonDecrypt.Text = "Decryption is done!"
+
+    End Sub
+
+    Private Sub buttonSelectInput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonSelectInput.Click
+        dlgInput.ShowDialog()
+        If dlgInput.FileName = "" Then Exit Sub
+        InputSelected = True
+        TextBox1.Text = dlgInput.FileName
+        auto = System.IO.Path.GetDirectoryName(TextBox1.Text) & "\" & _
+                   System.IO.Path.GetFileName(TextBox1.Text) & _
+                    "_decrypt.dmg"
+        TextBox2.Text = auto
+    End Sub
+
+
+    Private Sub buttonSelectOutput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonSelectOutput.Click
+        dlgOutput.ShowDialog()
+        If dlgOutput.FileName = "" Then Exit Sub
+        OutputSelected = True
+        TextBox2Text = dlgInput.FileName
+    End Sub
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        If Command() <> "" Then
+            TextBox1.Text = Command()
+            InputSelected = True
+            auto = System.IO.Path.GetDirectoryName(TextBox1.Text) & "\" & _
+                       System.IO.Path.GetFileNameWithoutExtension(TextBox1.Text) & _
+                       "_decrypted.dmg"
+            TextBox2.Text = auto
+        End If
+
+    End Sub
+
+    Private Sub tmrChecker_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrChecker.Tick
+        If InputSelected = True And OutputSelected = True Then
+            buttonDecrypt.Enabled = True
+        Else
+            buttonDecrypt.Enabled = False
+        End If
+    End Sub
+
+End Class
